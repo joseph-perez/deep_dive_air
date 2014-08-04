72faa00bb05c1edf6c8bb4cbbd590c3c1c12aab8
Joseph Perez <gear176@hotmail.com>
2014-08-04 11:06:52 -0600

    added php classes and login and sign up validation

---
+++ php/login_validation.php
@@ -0,0 +1,39 @@
+<?php
+    // require the classes needed
+    require_once("login.php");
+    
+    try
+    {
+        //enable exception handling
+        mysqli_report(MYSQLI_REPORT_STRICT);
+        
+        // connect to mySQL
+        $mysqli = new mysqli("", "", "", "");
+        
+        // get the users email from mySQL and the input email
+        $login = Login::getLoginByEmail($mysqli, $_POST["loginEmail"]);
+        
+        // get the user's salt
+        $loginSalt = $login->getSalt();
+        
+        // remake the hash and get the users password for comparison
+        $inputPassword = hash_pbkdf2("sha512", $_POST["loginPassword"], $loginSalt, 2050);
+        $loginPassword = $login->getPassword();
+        
+        // now compare the input password and login password to verify login
+        if($inputPassword === $loginPassword)
+        {
+            header("location:/profile.html");
+            exit;
+        }
+        else
+        {
+            echo "Your password or email must be incorrect!";
+        }
+    }
+    catch(mysqli_sql_exception $exception)
+    {
+        echo "Exception " . $exception->getMessage();
+    }
+    
+?>
